7061f183976107dd06d89eefc04ad112ef4fc478
Marcio Jorge Medeiros Melo Filho <marcio.melo@livsaude.com.br>
2025-06-26 14:11:10 -0300

    refactor: reajust images path

---
+++ src/shared/mocks/stacks.ts
@@ -0,0 +1,44 @@
+import figmaIcon from "../../../public/Figma.png";
+import javascriptIcon from "../../../public/JavaScript.png";
+import typescriptIcon from "../../../public/TypeScript.png";
+import htmlIcon from "../../../public/Html 5.png";
+import cssIcon from "../../../public/CSS3.png";
+import sassIcon from "../../../public/Sass.png";
+import reactIcon from "../../../public/React.png";
+import NextIcon from "../../../public/Next.js.png";
+import tailwindIcon from "../../../public/Tailwindcss.png";
+import bitbucketIcon from "../../../public/Bitbucket.png";
+import githubIcon from "../../../public/Github-oficial.png";
+import jestIcon from "../../../public/Jest.png";
+import nodeIcon from "../../../public/Nodejs.png";
+import expressIcon from "../../../public/ExpressJs.png";
+import postgresIcon from "../../../public/PostgreSQL.png";
+import mongoIcon from "../../../public/MongoDb.png";
+import firebaseIcon from "../../../public/Firebase.png";
+import clickupIcon from "../../../public/ClickUp.png";
+import dockerIcon from "../../../public/Docker.png";
+import gitIcon from "../../../public/Git.png";
+import { StaticImageData } from "next/image";
+
+export const stacks: StaticImageData[] = [
+  reactIcon,
+  NextIcon,
+  typescriptIcon,
+  jestIcon,
+  figmaIcon,
+  tailwindIcon,
+  sassIcon,
+  cssIcon,
+  htmlIcon,
+  javascriptIcon,
+  dockerIcon,
+  nodeIcon,
+  expressIcon,
+  postgresIcon,
+  mongoIcon,
+  firebaseIcon,
+  githubIcon,
+  gitIcon,
+  bitbucketIcon,
+  clickupIcon,
+];
