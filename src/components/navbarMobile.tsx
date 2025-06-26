98219ab76513ca95ca42fd7fc4ef8c1ac561735d
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-06-25 22:06:02 -0300

    style: reajust width´s navbar items and icons

--- src/components/navbarMobile.tsx
+++ src/components/navbarMobile.tsx
@@ -1,55 +1,51 @@
-import {
-  BriefcaseBusiness,
-  CodeXml,
-  House,
-  Info,
-  SquareCode,
-} from "lucide-react";
+import { BriefcaseBusiness, House, Layers } from "lucide-react";
+import { BiMailSend } from "react-icons/bi";
+import { RxInfoCircled } from "react-icons/rx";
 
 const NavbarMobile = () => {
   return (
-    <nav className="fixed bottom-0 left-0 w-full bg-[#1B1F24] z-50 px-6 h-20 flex gap-6 items-center justify-between border-t border-[#2D343F]">
+    <nav className="fixed bottom-0 left-0 w-full bg-dark-background z-50 px-6 py-3.5 flex gap-6 items-center justify-between border-t border-[#2D343F]">
       <a
         href="#presentation"
-        className="text-[#1B98FE] flex flex-1 flex-col items-center gap-1.5"
+        className="text-secondary-blue w-[57px] flex flex-col items-center justify-center gap-1.5"
       >
-        <House size={24} />
+        <House size={32} />
 
         <p className="text-[10px]">Início</p>
       </a>
 
       <a
         href="#experience"
-        className="text-white flex flex-1 flex-col items-center gap-1.5"
+        className="text-white w-[57px] flex flex-col items-center justify-center gap-1.5"
       >
-        <BriefcaseBusiness size={24} />
+        <BriefcaseBusiness size={32} />
 
         <p className="text-[10px]">Experiência</p>
       </a>
 
       <a
         href="#projects"
-        className="text-white flex flex-1 flex-col items-center gap-1.5"
+        className="text-white w-[57px] flex flex-col items-center justify-center gap-1.5"
       >
-        <SquareCode size={24} />
+        <Layers size={32} />
 
         <p className="text-[10px]">Projetos</p>
       </a>
 
       <a
         href="#about"
-        className="text-white flex flex-1 flex-col items-center gap-1.5"
+        className="text-white w-[57px] flex flex-col items-center justify-center gap-1.5"
       >
-        <Info size={24} />
+        <RxInfoCircled size={32} />
 
         <p className="text-[10px]">Sobre</p>
       </a>
 
       <a
         href="#contact"
-        className="text-white flex flex-1 flex-col items-center gap-1.5"
+        className="text-white w-[57px] flex flex-col items-center justify-center gap-1.5"
       >
-        <CodeXml size={24} strokeWidth={2.5} />
+        <BiMailSend size={32} />
 
         <p className="text-[10px]">Contato</p>
       </a>
