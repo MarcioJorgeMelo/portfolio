f133f8a97e3e6b0d826439c3476ceaaf6e3d3d37
Marcio Jorge Medeiros Melo Filho <marcio.melo@livsaude.com.br>
2025-06-26 14:14:55 -0300

    refactor: use links to renderize links

--- src/components/navbarMobile.tsx
+++ src/components/navbarMobile.tsx
@@ -1,54 +1,27 @@
-import { BriefcaseBusiness, House, Layers } from "lucide-react";
-import { BiMailSend } from "react-icons/bi";
-import { RxInfoCircled } from "react-icons/rx";
+"use client"
+
+import { useActiveSection } from "@/shared/hooks/useActiveSection";
+import { links } from "@/shared/mocks/links";
+import clsx from "clsx";
 
 const NavbarMobile = () => {
+  const activeSection = useActiveSection(links.map(item => { return item.id }));
+
   return (
-    <nav className="fixed bottom-0 left-0 w-full bg-dark-background z-50 px-6 py-3.5 flex gap-6 items-center justify-between border-t border-[#2D343F]">
-      <a
-        href="#presentation"
-        className="text-secondary-blue w-[57px] flex flex-col items-center justify-center gap-1.5"
-      >
-        <House size={32} />
-
-        <p className="text-[10px]">Início</p>
-      </a>
-
-      <a
-        href="#experience"
-        className="text-white w-[57px] flex flex-col items-center justify-center gap-1.5"
-      >
-        <BriefcaseBusiness size={32} />
-
-        <p className="text-[10px]">Experiência</p>
-      </a>
-
-      <a
-        href="#projects"
-        className="text-white w-[57px] flex flex-col items-center justify-center gap-1.5"
-      >
-        <Layers size={32} />
-
-        <p className="text-[10px]">Projetos</p>
-      </a>
-
-      <a
-        href="#about"
-        className="text-white w-[57px] flex flex-col items-center justify-center gap-1.5"
-      >
-        <RxInfoCircled size={32} />
-
-        <p className="text-[10px]">Sobre</p>
-      </a>
-
-      <a
-        href="#contact"
-        className="text-white w-[57px] flex flex-col items-center justify-center gap-1.5"
-      >
-        <BiMailSend size={32} />
-
-        <p className="text-[10px]">Contato</p>
-      </a>
+    <nav className="w-full bg-dark-background px-6 py-3.5 flex gap-6 items-center justify-between border-t border-[#2D343F]">
+      {links.map(link => (
+        <a
+          key={link.id}
+          href={link.href}
+          className={clsx(
+            "w-[57px] flex flex-col items-center justify-center gap-1.5",
+            activeSection === link.id ? "text-secondary-blue" : "text-white"
+          )}
+        >
+          <link.icon size={32} />
+          <p className="text-[10px]">{link.label}</p>
+        </a>
+      ))}
     </nav>
   );
 };
