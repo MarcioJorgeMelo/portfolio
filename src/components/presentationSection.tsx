c566318ec04208e46a4c46dcc6c6e1e3765d7938
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-06-25 22:08:39 -0300

    feat: reajusting style to new Apresentation section´s design and add interactive on many of contacts

--- src/components/presentationSection.tsx
+++ src/components/presentationSection.tsx
@@ -1,151 +1,127 @@
-import { ArrowRight, ChevronDown, FileDown } from "lucide-react";
-import Link from "next/link";
+import { CodeXml, LoaderCircle } from "lucide-react";
 import Image from "next/image";
-import apresentationImage from "../../public/apresentationImg.jpg";
+import apresentationRoundedImage from "../../public/@rafasdoliveira-43.png";
 import linkedinIcon from "../../public/LinkedIn.png";
 import githubIcon from "../../public/GitHub.png";
 import whatsappIcon from "../../public/WhatsApp.png";
 import instagramIcon from "../../public/Instagram.png";
-import emailIcon from "../../public/Email.png";
-import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
+import EmailButton from "./emailButton";
+import CurriculumDownloadButton from "./curriculumDownload";
 
 const Presentation = () => {
   return (
-    <section id="#presentation" className="pb-10">
-      <aside className="relative h-[450px]">
-        <Image
-          src={apresentationImage}
-          alt="Foto pessoal do Márcio Jorge"
-          quality={100}
-          priority
-          fill
-          className="object-cover"
+    <section
+      id="#presentation"
+      className="w-full py-10 px-6 flex flex-col gap-3"
+    >
+      <aside className="w-full flex justify-center relative items-center gap-7">
+        <Avatar className="w-[150px] h-[150px] rounded-full">
+          <AvatarImage
+            src={apresentationRoundedImage.src}
+            alt="Imagem do Márcio Jorge"
+            className="object-cover"
+          />
+          <AvatarFallback className="bg-dark-background">
+            <LoaderCircle
+              size={64}
+              color="var(--color-primary-blue)"
+              className="animate-spin"
+            />
+          </AvatarFallback>
+        </Avatar>
+
+        <CodeXml
+          size={36}
+          strokeWidth={3}
+          color="var(--color-primary-blue)"
+          className="absolute -bottom-2 left-26 z-10"
         />
+
+        <div className="w-full flex flex-col items-start gap-2.5">
+          <div className="w-full max-w-[96px] bg-gradient-to-r from-primary-blue to-secondary-blue h-[5px]" />
+
+          <h1 className="text-white text-[28px] font-bold">
+            Márcio Jorge,
+            <br />
+            Desenvolvedor Frontend
+          </h1>
+        </div>
       </aside>
 
-      <div className="px-6 pb-12 flex flex-col w-full gap-6">
-        <aside className="pt-10 flex flex-col gap-10">
-          <div>
-            <div className="w-full max-w-[96px] bg-white h-2 mb-6" />
-            <h1 className="text-white text-[34px] font-bold mb-3">
-              Márcio Jorge, Desenvolvedor Frontend
-            </h1>
-            <p className="text-[#C6C6C6] leading-8 font-medium">
-              Desenvolvedor Frontend com foco em UI/UX, especialista em
-              desenvolver interfaces modernas, intuitivas e de grande impacto.
-              Uno criatividade e domínio técnico para transformar ideias em
-              experiências digitais que encantam e geram resultados.
-            </p>
+      <aside className="py-6 flex flex-col w-full">
+        <p className="text-text-secondary leading-8 font-medium">
+          Desenvolvedor Frontend com foco em UI/UX, especialista em desenvolver
+          interfaces modernas, intuitivas e de grande impacto. Uno criatividade
+          e domínio técnico para transformar ideias em experiências digitais que
+          encantam e geram resultados.
+        </p>
+      </aside>
+
+      <aside className="w-full flex items-center justify-center gap-6 pt-6">
+        <div className="w-full flex flex-col gap-6">
+          <h3 className="text-white text-xss font-bold">ENTRE EM CONTATO</h3>
+
+          <div className="flex gap-5 items-center">
+            <Link
+              href="https://www.linkedin.com/in/m%C3%A1rcio-jorge-417aa127a/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src={linkedinIcon}
+                alt="Ícone do LinkedIn"
+                height={28}
+                width={28}
+              />
+            </Link>
+
+            <EmailButton />
+
+            <Link
+              href="https://wa.me/5585985603089?text=Olá, vim pelo seu porfólio!"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src={whatsappIcon}
+                alt="Ícone do Whatsapp"
+                height={28}
+                width={28}
+              />
+            </Link>
+
+            <Link
+              href="https://www.instagram.com/jorge.developer/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src={instagramIcon}
+                alt="Ícone do Instagram"
+                height={28}
+                width={28}
+              />
+            </Link>
+
+            <Link
+              href="https://github.com/MarcioJorgeMelo"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src={githubIcon}
+                alt="Ícone do Github"
+                height={28}
+                width={28}
+              />
+            </Link>
           </div>
-          <Button className="bg-[#0760E2] h-16 w-16 rounded-full flex items-center justify-center">
-            <ChevronDown
-              className="size-10 animate-bounce"
-              strokeWidth={2.5}
-              color="#FFF"
-            />
-          </Button>
-        </aside>
+        </div>
 
-        <aside className="w-full flex flex-col gap-6">
-          <div className="w-full flex flex-col gap-[18px]">
-            <h3 className="text-white text-base font-bold">SOBRE MIM</h3>
-
-            <p className="text-[#C6C6C6] leading-8 font-medium">
-              Atuação no time de tecnologia da Livsaúde, desenvolvendo sites e
-              sistemas internos com foco em performance e usabilidade.
-              Experiência com{" "}
-              <span className="font-bold text-white">React</span>,{" "}
-              <span className="font-bold text-white">Figma</span> e{" "}
-              <span className="font-bold text-white">Electron</span> na criação
-              de interfaces.
-            </p>
-
-            <div className="w-fit flex items-center gap-3 pb-1.5 text-sm font-bold text-white border-b-[1.5px] border-[#445166]">
-              SAIBA MAIS
-              <ArrowRight size={16} color="#FFF" />
-            </div>
-          </div>
-
-          <div className="w-full h-[1px] bg-[#2D343F] rounded-full" />
-
-          <div className="w-full flex flex-col gap-[18px]">
-            <h3 className="text-white text-base font-bold">MEUS PROJETOS</h3>
-
-            <p className="text-[#C6C6C6] leading-8 font-medium">
-              Atuação no time de tecnologia da Livsaúde, desenvolvendo sites e
-              sistemas internos com foco em performance e usabilidade.
-              Experiência com{" "}
-              <span className="font-bold text-white">React</span>,{" "}
-              <span className="font-bold text-white">Figma</span> e{" "}
-              <span className="font-bold text-white">Electron</span> na criação
-              de interfaces.
-            </p>
-
-            <div className="w-fit flex items-center gap-3 pb-1.5 text-sm font-bold text-white border-b-[1.5px] border-[#445166]">
-              VER PROJETOS
-              <ArrowRight size={16} color="#FFF" />
-            </div>
-          </div>
-
-          <div className="w-full h-[1px] bg-[#2D343F] rounded-full" />
-
-          <div className="w-full flex flex-col gap-[18px]">
-            <h3 className="text-white text-base font-bold">ENTRE EM CONTATO</h3>
-
-            <div className="flex gap-5 items-center">
-              <Link href="#">
-                <Image
-                  src={linkedinIcon}
-                  alt="Ícone do LinkedIn"
-                  height={24}
-                  width={24}
-                />
-              </Link>
-
-              <Link href="#">
-                <Image
-                  src={githubIcon}
-                  alt="Ícone do Github"
-                  height={24}
-                  width={24}
-                />
-              </Link>
-
-              <Link href="#">
-                <Image
-                  src={whatsappIcon}
-                  alt="Ícone do Whatsapp"
-                  height={24}
-                  width={24}
-                />
-              </Link>
-
-              <Link href="#">
-                <Image
-                  src={instagramIcon}
-                  alt="Ícone do Instagram"
-                  height={24}
-                  width={24}
-                />
-              </Link>
-
-              <Link href="#">
-                <Image
-                  src={emailIcon}
-                  alt="Ícone do Email"
-                  height={24}
-                  width={24}
-                />
-              </Link>
-            </div>
-
-            <Button className="w-fit text-[18px] font-bold bg-[#0760E2] flex items-center gap-3">
-              Baixar currículo
-              <FileDown className="size-6" />
-            </Button>
-          </div>
-        </aside>
-      </div>
+        <CurriculumDownloadButton />
+      </aside>
     </section>
   );
 };
