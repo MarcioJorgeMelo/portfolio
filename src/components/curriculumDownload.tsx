de4a0b0627f06f5913add8ec2ea70fc341aceb84
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-06-26 21:42:02 -0300

    feat: add new type for CurriculumDownloadButton

--- src/components/curriculumDownload.tsx
+++ src/components/curriculumDownload.tsx
@@ -2,16 +2,28 @@
 
 import { FileDown } from "lucide-react";
 import { Button } from "./ui/button";
+import { CurriculumDownloadButtonEnum } from "@/@types/curriculumDownloadButtonEnum";
 
-const CurriculumDownloadButton = () => {
+interface CurriculumDownloadButtonProps {
+  type: CurriculumDownloadButtonEnum;
+}
+
+const CurriculumDownloadButton = ({ type }: CurriculumDownloadButtonProps) => {
   return (
     <a href="/curriculo_marcio_jorge_ats.pdf" download>
-      <Button variant="download" size="download">
-        <FileDown className="size-6" />
-        Baixar
-        <br />
-        currículo
-      </Button>
+      {type === CurriculumDownloadButtonEnum.COL ? (
+        <Button variant="download" size="download">
+          <FileDown className="size-6" />
+          Baixar
+          <br />
+          currículo
+        </Button>
+      ) : (
+        <Button variant="seeMore" size="seeMore" className="gap-3">
+          Baixar currículo
+          <FileDown className="size-6" />
+        </Button>
+      )}
     </a>
   );
 };
