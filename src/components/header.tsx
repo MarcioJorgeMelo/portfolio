89082c6f0234ae7ee381f3c3cae1b3807a49d68e
Marcio Jorge Medeiros Melo Filho <marcio.melo@livsaude.com.br>
2025-06-30 13:50:02 -0300

    feat(header.tsx): add new funtions with inactivity timeout, adjust condicional styles and more...

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,30 +1,71 @@
 "use client";
 
 import { navbarOptions } from "@/shared/mocks/navbarOptions";
+import clsx from "clsx";
 import { CodeXml } from "lucide-react";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Header = () => {
   const [hoveredId, setHoveredId] = useState<string | null>(null);
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
+    let lastScrollY = window.scrollY;
+
     const handleScroll = () => {
-      setIsScrolled(window.scrollY > 50);
+      const currentScrollY = window.scrollY;
+
+      setIsScrolled(currentScrollY > 50);
+
+      const isScrollingDown = currentScrollY > lastScrollY && currentScrollY > 50;
+
+      if (isScrollingDown) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+
+        if (timeoutRef.current) clearTimeout(timeoutRef.current);
+        timeoutRef.current = setTimeout(() => {
+          if (window.scrollY < 50) return;
+          setIsVisible(false);
+        }, 5000);
+      }
+
+      lastScrollY = currentScrollY;
     };
 
     window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    };
   }, []);
 
   return (
     <header
-      className={`fixed top-0 left-1/2 -translate-x-1/2 z-50 w-full max-w-[1536px] mx-auto flex justify-between items-center px-6 py-9 transition-all duration-300 ${
-        isScrolled
-          ? "w-[1024px] p-6 rounded-3xl mt-6 bg-[#1B1E23] backdrop-blur-md shadow-lg ring-1 ring-[#999999]"
-          : ""
-      }`}
+      className={clsx(
+        "fixed top-0 left-1/2 -translate-x-1/2 z-50 w-full mx-auto flex justify-between items-center transition-all duration-300",
+        {
+          "max-w-[1536px] pr-6 py-9": !isScrolled,
+          "max-w-[1024px] p-6 rounded-3xl mt-6 bg-[#1B1E23] ring-1 ring-[#999999]": isScrolled,
+          "opacity-0 -translate-y-full pointer-events-none": !isVisible,
+          "opacity-100 translate-y-0": isVisible,
+        }
+      )}
+      onMouseEnter={() => {
+        if (timeoutRef.current) clearTimeout(timeoutRef.current);
+      }}
+      onMouseLeave={() => {
+        if (window.scrollY > 50) {
+          timeoutRef.current = setTimeout(() => {
+            setIsVisible(false);
+          }, 5000)
+        }
+      }}
     >
       <Link
         href="#"
@@ -32,27 +73,26 @@ const Header = () => {
           e.preventDefault();
           window.scrollTo({ top: 0, behavior: "smooth" });
         }}
-        className="flex items-center gap-3 hover:scale-110 duration-300"
+        className="flex items-center gap-3 hover:scale-105 duration-300"
       >
-        <CodeXml className="w-10 h-10 text-primary-blue" strokeWidth={3} />
+        <CodeXml className={clsx("text-primary-blue", { "w-10 h-10": !isScrolled, "w-8 h-8": isScrolled })} strokeWidth={3} />
 
-        <h1 className="text-[28px] text-white font-bold">Márcio Jorge</h1>
+        <h1 className={clsx("text-white font-bold", { "text-[28px]": !isScrolled, "text-2xl": isScrolled })}>Márcio Jorge</h1>
       </Link>
 
-      <nav className="flex items-center gap-9">
+      <nav className={clsx("flex items-center", { "gap-9": !isScrolled, "gap-6": isScrolled })}>
         {navbarOptions.map((option) => (
           <Link
             href={option.href}
             key={option.id}
             onMouseEnter={() => setHoveredId(option.id)}
             onMouseLeave={() => setHoveredId(null)}
-            className={`transition-opacity duration-300 ${
-              hoveredId && hoveredId !== option.id
-                ? "opacity-30"
-                : "opacity-100"
-            }`}
+            className={`transition-opacity duration-500 ${hoveredId && hoveredId !== option.id
+              ? "opacity-30"
+              : "opacity-100"
+              }`}
           >
-            <h3 className="text-[20px] text-text-secondary font-medium hover:text-white">
+            <h3 className={clsx("text-text-secondary font-medium hover:text-white", { "text-[20px]": !isScrolled, "text-lg": isScrolled })}>
               {option.label}
             </h3>
           </Link>
