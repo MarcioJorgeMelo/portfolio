3a25b99035845bd22040849555c257160f0d4a8f
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-07-06 18:05:44 -0300

    feat: create footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="flex flex-col gap-4 items-center justify-center py-3">
+      <div className="w-[660px] h-[1px] bg-[#2D343F]" />
+
+      <span className="text-[#999999]">
+        @ 2025 Márcio Jorge. Todos os direitos reservados.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
