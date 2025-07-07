8dbc3b46488437f29e90547a48a1cd2b165ece1b
Marcio Jorge Medeiros Melo Filho <marcio.melo@livsaude.com.br>
2025-07-07 14:11:43 -0300

    feat(tmp): add translation on CurriculumDownloadButton

--- src/components/curriculumDownload.tsx
+++ src/components/curriculumDownload.tsx
@@ -3,24 +3,27 @@
 import { FileDown } from "lucide-react";
 import { Button } from "./ui/button";
 import { CurriculumDownloadButtonEnum } from "@/@types/curriculumDownloadButtonEnum";
+import { useTranslations } from "next-intl";
 
 interface CurriculumDownloadButtonProps {
   type: CurriculumDownloadButtonEnum;
 }
 
 const CurriculumDownloadButton = ({ type }: CurriculumDownloadButtonProps) => {
+  const t = useTranslations("Presentation");
+
   return (
     <a href="/curriculo_marcio_jorge_ats.pdf" download>
       {type === CurriculumDownloadButtonEnum.COL ? (
         <Button variant="download" size="download">
           <FileDown className="size-6" />
-          Baixar
+          {t("col-button1")}
           <br />
-          currículo
+          {t("col-button2")}
         </Button>
       ) : (
         <Button variant="seeMore" size="seeMore" className="gap-3">
-          Baixar currículo
+          {t("row-button")}
           <FileDown className="size-6" />
         </Button>
       )}
