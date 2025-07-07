3efa66c0d5a26284ff57a7e271dc00d588344eaf
Marcio Jorge Medeiros Melo Filho <marcio.melo@livsaude.com.br>
2025-07-07 14:08:51 -0300

    feat: fix the lastScroll header bug when F5 on page and add translation language

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,31 +1,32 @@
 "use client";
 
-import { navbarOptions } from "@/shared/mocks/navbarOptions";
+import { useNavigationOptions } from "@/shared/mocks/navigationOptions";
 import clsx from "clsx";
 import { ArrowLeftRight, CodeXml } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import { Button } from "./ui/button";
-import { languagesOptions } from "@/shared/mocks/languageOptions";
+import { useTranslations } from "next-intl";
 
 const Header = () => {
+  const t = useTranslations("TranslateOption");
+
+  const navigationOptions = useNavigationOptions();
+
   const [hoveredId, setHoveredId] = useState<string | null>(null);
   const [isScrolled, setIsScrolled] = useState(false);
   const [isVisible, setIsVisible] = useState(true);
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-  const [translateEnglish, setTranslateEnglish] = useState(false);
 
   useEffect(() => {
     let lastScrollY = window.scrollY;
 
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
-
       setIsScrolled(currentScrollY > 50);
 
-      const isScrollingDown =
-        currentScrollY > lastScrollY && currentScrollY > 50;
+      const isScrollingDown = currentScrollY > lastScrollY && currentScrollY > 50;
 
       if (isScrollingDown) {
         setIsVisible(false);
@@ -42,6 +43,8 @@ const Header = () => {
       lastScrollY = currentScrollY;
     };
 
+    setIsScrolled(window.scrollY > 50);
+
     window.addEventListener("scroll", handleScroll);
 
     return () => {
@@ -50,17 +53,14 @@ const Header = () => {
     };
   }, []);
 
+
   return (
     <header
       className={clsx(
-        "fixed top-0 left-1/2 -translate-x-1/2 z-50 w-full mx-auto flex justify-between items-center transition-all duration-300",
-        {
-          "max-w-[1536px] pr-6 py-9": !isScrolled,
-          "max-w-[1024px] p-6 rounded-3xl mt-6 bg-[#1B1E23] ring-1 ring-[#999999]":
-            isScrolled,
-          "opacity-0 -translate-y-full pointer-events-none": !isVisible,
-          "opacity-100 translate-y-0": isVisible,
-        }
+        "fixed top-0 left-1/2 -translate-x-1/2 z-50 w-full mx-auto flex justify-between items-center ring-1 transition-all duration-300 will-change-transform will-change-opacity",
+        "px-6",
+        isScrolled ? "max-w-[1024px] py-6 mt-6 rounded-3xl bg-[#1B1E23] ring-[#999999]" : "max-w-[1536px] py-9 ring-transparent",
+        !isVisible ? "opacity-0 -translate-y-full pointer-events-none" : "opacity-100 translate-y-0"
       )}
       onMouseEnter={() => {
         if (timeoutRef.current) clearTimeout(timeoutRef.current);
@@ -82,7 +82,7 @@ const Header = () => {
         className="flex items-center gap-3 hover:scale-105 duration-300"
       >
         <CodeXml
-          className={clsx("text-primary-blue", {
+          className={clsx("text-primary-blue transition-all duration-300", {
             "w-10 h-10": !isScrolled,
             "w-8 h-8": isScrolled,
           })}
@@ -90,7 +90,7 @@ const Header = () => {
         />
 
         <h1
-          className={clsx("text-white font-bold", {
+          className={clsx("text-white font-bold transition-all duration-300", {
             "text-[28px]": !isScrolled,
             "text-2xl": isScrolled,
           })}
@@ -105,17 +105,16 @@ const Header = () => {
           "gap-6": isScrolled,
         })}
       >
-        {navbarOptions.map((option) => (
+        {navigationOptions.map((option) => (
           <Link
             href={option.href}
             key={option.id}
             onMouseEnter={() => setHoveredId(option.id)}
             onMouseLeave={() => setHoveredId(null)}
-            className={`transition-opacity duration-500 ${
-              hoveredId && hoveredId !== option.id
-                ? "opacity-30"
-                : "opacity-100"
-            }`}
+            className={`transition-opacity duration-500 ${hoveredId && hoveredId !== option.id
+              ? "opacity-30"
+              : "opacity-100"
+              }`}
           >
             <h3
               className={clsx(
@@ -134,23 +133,19 @@ const Header = () => {
           className={clsx("inline-flex items-center", {
             hidden: isScrolled,
           })}
-          onClick={() => setTranslateEnglish(!translateEnglish)}
+          asChild
         >
-          <Image
-            src={
-              translateEnglish
-                ? languagesOptions[0].image
-                : languagesOptions[1].image
-            }
-            alt={
-              translateEnglish
-                ? languagesOptions[0].alt
-                : languagesOptions[1].alt
-            }
-            className="size-6 rounded-full"
-          />
+          <Link href={t("language")}>
+            <Image
+              src={t("image")}
+              alt={t("alt")}
+              width={24}
+              height={24}
+              className="size-6 rounded-full"
+            />
 
-          <ArrowLeftRight className="size-5" color="#FFF" />
+            <ArrowLeftRight className="size-5" color="#FFF" />
+          </Link>
         </Button>
       </nav>
     </header>
